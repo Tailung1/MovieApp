e84cf01ce4358cd7b40daa896e9556569d2ac29b
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-16 13:10:47 +0400

    css/html Done for Login

---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
+export default router
