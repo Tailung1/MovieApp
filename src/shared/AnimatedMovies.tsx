e56ffbd9db33be78b107177fb7eed5a6f7d1b8b2
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-21 18:12:56 +0400

    header title for movies in shared component animatedMovies is DONE

---
+++ src/shared/AnimatedMovies.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useSearchMovie } from "../MovieContext";
+import data from "../movies.json";
+
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function AnimatedMovies() {
+  const { searchMovie } = useSearchMovie();
+  const [movieMatches, setMovieMatches] = useState<boolean>(false);
+  const [filtered, setFiltered] = useState(data);
+
+  useEffect(() => {
+    let filtered = data;
+
+    setMovieMatches(true);
+  }, [searchMovie]);
+
+  return (
+    <div>
+      <h2 className='text-white pb-[15px] text-[20px] min-h-[24px]'>
+        {searchMovie ? (
+          !movieMatches ? (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              className='text-red-400 italic'
+            >
+              no Series Found
+            </motion.span>
+          ) : (
+            "Filtered Series"
+          )
+        ) : (
+          "series"
+        )}
+      </h2>
+      <div className='flex flex-wrap gap-[15px]'>
+        <AnimatePresence mode='popLayout'></AnimatePresence>
+      </div>
+    </div>
+  );
+}
