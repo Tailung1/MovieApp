bc7dc5c611f101a400bd0ac345e3b5a3bf2a5916
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-21 11:56:49 +0400

    improve display of cotnext code

--- src/MovieContext.tsx
+++ src/MovieContext.tsx
@@ -1,4 +1,9 @@
-import React, { ReactNode, createContext, useContext, useState } from "react";
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useState,
+} from "react";
 
 interface IsearchMovie {
   searchMovie: string;
