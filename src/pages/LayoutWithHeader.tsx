d85c07bc43c365774c8deed8ae71894eaf155ec5
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-29 22:08:57 +0400

    create layoutwithheader

---
+++ src/pages/LayoutWithHeader.tsx
@@ -0,0 +1,19 @@
+// import Header from "../shared/Header";
+// import Input from "../shared/Input";
+// import { Outlet } from "react-router-dom";
+
+// export default function LayoutWithHeader() {
+//   return (
+//     // On large screens, flex row. On small screens, stack by default.
+//     <div className='lg:flex lg:min-h-screen'>
+//       {/* Header: fixed width on large screens */}
+//       <Header className='lg:w-1/4' />
+
+//       {/* Right side: input + page content */}
+//       <div className='flex-1'>
+//         <Input />
+//         <Outlet />
+//       </div>
+//     </div>
+//   );
+// }
